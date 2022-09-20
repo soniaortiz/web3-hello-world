dd99e55f3d70f1d72e90069b9720cee42cecd5a3
Remix Workspace <>
2022-09-19 22:29:41 -0500

    init

---
+++ contracts/helloWorld.sol
@@ -0,0 +1,20 @@
+// SPDX-Licese-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract helloWorld{
+    //booleano
+    bool flagVerdadero = true;
+    bool flagFalso = false;
+
+    //Int
+
+    int numero1 = -2;
+
+    // para valores positivos o negativos
+    uint numero2 = 2;
+
+    address public addressAccount = addres(this);
+
+
+}
